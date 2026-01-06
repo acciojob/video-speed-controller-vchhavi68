743fadd87cfaa3c94c38623af311aae269146566
vchhavi68 <vchhavi68@gmail.com>
2026-01-06 22:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('video');
+const speed = document.querySelector('.speed');
+const speedBar = document.querySelector('.speed-bar');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function handleSpeed(e) {
+  
+  const x = e.offsetX;
+  const width = speed.offsetWidth;
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+  
+  let percent = x / width;
+	
+  if (percent < 0) percent = 0;
+  if (percent > 1) percent = 1;
+	
+  const minSpeed = 0.5;
+  const maxSpeed = 2;
+  const speedValue = (percent * (maxSpeed - minSpeed)) + minSpeed;
+
+  
+  video.playbackRate = speedValue;
+
+ 
+  speedBar.style.width = `${percent * 100}%`;
+  speedBar.textContent = `${speedValue.toFixed(2)}×`;
+}
+
+let isMouseDown = false;
+
+
+speed.addEventListener('mousemove', e => {
+  if (isMouseDown) handleSpeed(e);
+});
+
+speed.addEventListener('mousedown', e => {
+  isMouseDown = true;
+  handleSpeed(e);
+});
+
+document.addEventListener('mouseup', () => {
+  isMouseDown = false;
+});
